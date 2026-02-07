64e7805fc60b7e1776816052a08b3e3371de9cb5
vanshk5 <vanshkapoor.cu@gmail.com>
2026-02-07 11:48:43 -0500

    Update GoldenAura.js

--- src/pages/GoldenAura.js
+++ src/pages/GoldenAura.js
@@ -1,6 +1,16 @@
-import React from "react";
-
 export default function GoldenAura() {
-  return <div className="page">GoldenAura Page</div>;
-}
+  return (
+    <div className="card">
+      <h2>Your Golden Aura</h2>
 
+      <p>Total: 42</p>
+      <p>Walking: 15</p>
+      <p>Wellbeing: 10</p>
+      <p>Eco: 17</p>
+
+      <button className="btn-3d">
+        Play Recap
+      </button>
+    </div>
+  );
+}
